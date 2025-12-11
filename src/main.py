e7cd3e44104490eab535387f2040463de1d020b3
Bhags <bhagya.wanniarachchige@edfenergy.com>
2025-12-11 10:56:41 +0000

    feat: Added to main.py

--- src/main.py
+++ src/main.py
@@ -3,3 +3,4 @@ def print_message():
     print("This is an update to my python script to test the auto versioning bump function")
     print("This a further change to test the above print statment, but after write permissions were allowed")
     print("Adding to script to see if CHANGELOG is being updated, after fixing the issues")
+    print("Testing the new PR pipeline")
